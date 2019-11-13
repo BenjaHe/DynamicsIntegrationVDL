655125cd4ed8756cedc26d3723ec0ba856a4e421
Roland NEYRINCK <info@appalach.be>
2019-11-13 09:26:39 +0100

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Dynamics Integration VDL",
+
+    'summary': """
+        Extention du module StockVDL pour l'intégration avec Dynamics.""",
+
+    'description': """
+        Le module ajoute les informations nécessaires (champs) pour pousser automatiquement les commandes webshop
+        dans Microsoft Dynamics. Le protocole d'échange se basant sur des fichiers CSV, seuls des champs on été ajouté
+        mais aucun export n'a été programmé.
+
+    """,
+
+    'author': "Roland Neyrinck - DSI Ville de Liège",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale_stock', 'sale', 'purchase', 'account','website', 'website_sale','payment','payment_transfer','website','website_portal_sale'],
+
+    'data': [
+
+             ],
+
+}
+
