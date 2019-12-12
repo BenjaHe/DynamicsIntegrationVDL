86c5b84d8eb532b61710234583eefceec5755f07
Roland NEYRINCK <info@appalach.be>
2019-12-12 09:40:55 +0100

    ajout du no update dans le ficher data

--- models/res_partner.py
+++ models/res_partner.py
@@ -10,11 +10,16 @@ class ResPartner(models.Model):
     _inherit = ['res.partner']
 
     # Champs Dynamics qui donne les références Dyn du fournisseur
-    dyn_buyergroupid = fields.Char(string='Numéro du comptable dans Dynamics', required=False,
-                                   track_visibility='onchange', help='A renseigner uniquement pour les comptables.')
-    mon_dyn_buyergroupid = fields.Char(string='Numéro de mon comptable dans Dynamics', required=False,
+    dyn_buyergroupid = fields.Char(string='Numéro du comptable dans Dynamics',
+                                   required=False,
+                                   track_visibility='onchange',
+                                   help='A renseigner uniquement pour les comptables.')
+
+    mon_dyn_buyergroupid = fields.Char(string='Numéro de mon comptable dans Dynamics',
+                                       required=False,
                                        related='comptable.dyn_buyergroupid',
                                        help='Le numéro du comptable dans Dynamics qui m est renseigné.')
 
-    dyn_orderaccount = fields.Char(string='Numéro du fournisseur dans Dynamics', required=False,
+    dyn_orderaccount = fields.Char(string='Numéro du fournisseur dans Dynamics',
+                                   required=False,
                                    help='Vient automatiquement')
